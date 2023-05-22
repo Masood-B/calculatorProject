2a21803caa9da051d1f63218d91508e901ad1899
Masood-B <masoodbasardien123@gmail.com>
2023-05-22 14:05:41 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,33 @@
+let result = document.querySelector("#screen")
+
+// number
+function display(value){
+    result.value 
+    += value;
+}
+
+// remove
+document.querySelector("#remove").
+addEventListener("click", ()=>{
+    let output = result.value;
+    result.value = output.slice (0,-1);
+})
+
+// clear
+document.querySelector("#clear").
+addEventListener("click", ()=>{
+    let output = result.value;
+    result.value = "";
+})
+
+// sign
+document.querySelectorAll(".operator").forEach( (ops)=>{
+    ops.addEventListener("click", (e)=>{
+        result.value += e.target.innerText;
+    })
+})
+// equal
+document.querySelector("#addup").
+addEventListener("click", ()=>{
+    result.value = eval(result.value).toFixed(2);
+})
